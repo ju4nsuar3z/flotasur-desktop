b60444347968b634d68e090870cafda412fc2bd5
Juan Suarez <suarezjuanesteban1@gmail.com>
2026-09-17 18:36:30 -0300

    Shell de Electron para CrewBoard

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+const APP_URL = 'https://crewboard-gamma.vercel.app';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 860,
+    minWidth: 960,
+    minHeight: 640,
+    backgroundColor: '#001e40',
+    icon: path.join(__dirname, 'icon.ico'),
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL(APP_URL);
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
